a666111f72c9555a2ec1d8b3659b7e0cbc77d733
Bainurs <Bainurs@users.noreply.github.com>
2019-01-27 01:51:28 +0600

    front-end
    OMG second push
    footer update
    header update

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -1,5 +1,40 @@
-<footer class="bg-dark p-5">
-    <div>
-        Tilya chto ty pryatal eto ot menya!
+<footer class="bg-light border-10">
+    <div class="container pt-5">
+        <div class="row">
+            <div class="col-3">
+                <nav class="nav flex-column">
+                    <a class="nav-link text-secondary" href="#">О НАС</a>
+                    <a class="nav-link text-secondary" href="#">КОНТАКТЫ</a>
+                    <a class="nav-link text-secondary" href="#">FAQ</a>
+                    <a class="nav-link text-secondary" href="#">ОТЗЫВЫ О СЕРВЕСЕ</a>
+
+                    <h4 class="mt-4 text-primary">Врачам и клинике</h4>
+                    <a class="nav-link text-secondary" href="/">ЛИЧНЫЙ КАБИНЕТ</a>
+                </nav>
+            </div>
+
+            <div class="col-3">
+                <nav class="nav flex-column">
+                    <h4 class="text-primary">ПАЦИЕНТУ</h4>
+                    <a class="nav-link text-secondary" href="/">ВРАЧИ</a>
+                    <a class="nav-link text-secondary" href="/">УСЛУГИ ВРАЧЕЙ</a>
+                    <a class="nav-link text-secondary" href="/">КЛИНИКИ</a>
+                    <a class="nav-link text-secondary" href="/">ДИАГНОСТИКА</a>
+                    <a class="nav-link text-secondary" href="/">БЛОГ</a>
+                </nav>
+            </div>
+            <div class="col-3">
+                <nav class="nav flex-column">
+                    <a href="/"><img class="w-75" src={{asset('img/logo.png')}} alt=""></a>
+                    <p class="pt-4 mb-0 text-secondary">Наши Контакты: </p>
+                    <p class="text-secondary">Наши Телефоны: </p>
+                </nav>
+            </div>
+        </div>
     </div>
 </footer>
+
+
+@push('styles')
+
+@endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     @stack('styles')
 </head>
 <body>
