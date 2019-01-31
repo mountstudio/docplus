5c10d314ddf60b2ade3c5de29b25db40712f959d
Bainurs <Bainurs@users.noreply.github.com>
2019-01-31 11:17:30 +0600

    front-end web clinik

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -3,22 +3,67 @@
 @section('content')
 
     <div class="container">
-        <div class="row">
-
-            @for($i = 0; $i < 6; $i++)
-                <div class="col-4 my-2">
-                    <div class="card">
-                        <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
+        <div class="d-flex justify-content-center">
+            <div class="col-6 pr-0">
+                <div class="btn-group ">
+                    <button type="button" class="btn shadow my-5 bg-light rounded dropdown-btn" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                         Action
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Separated link</a>
                     </div>
+                    <img class="flex-shrink-1 size-toggle my-5" src="{{ asset('img/dropdown-toggle.png') }}" alt="">
                 </div>
-            @endfor
+
+            </div>
+            <div class="col-6">
+                <button type="button" class="btn btn-primary h3 position-relative my-5">Найти</button>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-auto">
+                <p class="text-primary font-weight-bold mt-3 h4">СТОМАТОЛОГИ БИШКЕКА <span class="text-secondary font-weight-light">17</span></p>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                    <label class="form-check-label mr-4" for="defaultCheck1">
+                        Default checkbox
+                    </label>
+                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                    <label class="form-check-label" for="defaultCheck1">
+                        Default checkbox
+                    </label>
+                </div>
+
+            </div>
+
+
+        </div>
+        <div class="row">
+            <div class="col-auto">
+                <p class="pt-3 m-0">СОРТИРОВАТЬ</p>
+                <nav class="nav">
+                    <a class="nav-link btn btn-outline-secondary" href="#">Популярное</a>
+                    <a class="nav-link btn btn-outline-secondary" href="#">Рейтинг</a>
+                    <a class="nav-link btn btn-outline-secondary" href="#">Отзывы</a>
+                    <a class="nav-link btn btn-outline-secondary" href="#">Стаж</a>
+                    <a class="nav-link btn btn-outline-secondary" href="#">Стоимость</a>
+                </nav>
+            </div>
         </div>
     </div>
 
+    <div class="container">
+        <div class="row">
+            <div class="col-8">
+                
+            </div>
+        </div>
+    </div>
 
 @endsection
