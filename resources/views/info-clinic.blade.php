63384061b634b2b7162eb0767242e6928fc09044
Bainurs <Bainurs@users.noreply.github.com>
2019-02-07 17:24:38 +0600

    Update info-clinic.blade.php

--- resources/views/info-clinic.blade.php
+++ resources/views/info-clinic.blade.php
@@ -64,31 +64,58 @@
                </div>
                <div class="col-12">
                    <div class="row justify-content-center">
-                       <div class="col-8 bg-light shadow rounded py-3">
-                           <div class="row">
-                               <div class="col-3 text-center">
-                                   <div class="position-relative">
+                       <div class="col-8 bg-light  rounded py-3">
 
-                                       <img class="img-fluid rounded-circle mb-2 img-thumbnail " src="{{ asset('img/doctor.png') }}" alt="">
-                                   </div>
+                            <div class="col-12">
+                                <div class="row ">
 
-                                   @for($i = 0; $i < 5; $i++)
+                                    <div class="col-9">
+                                        <div class="row">
+                                            <div class="col-6 text-center">
+                                                <div class="position-relative">
 
-                                       <img class="star" src="{{ asset('img/star.png') }}" alt="">
+                                                    <img class="img-fluid rounded-circle mb-2 img-thumbnail " src="{{ asset('img/doctor.png') }}" alt="">
+                                                </div>
 
-                                   @endfor
-                                   <p class="text-secondary mt-3">Превосходный врач<br>на основе 171 отзыв</p>
-                               </div>
-                               <div class="col-6">
-                                   <p class="text-secondary h3 mt-5 mb-2">Бобров Василий Елисеевич</p>
-                                   <p class="text-secondary font-weight-light h6 my-3"><em>Гастроэнтеролог, Терапевт <br> Стаж 19 лет</em></p>
-                                   <p class="text-secondary font-weight-light mt-2 mb-5">Приём от <del>1400</del> руб.  <span class="text-primary font-weight-bold">1000 руб.</span></p>
-                                   <p class="text-secondary font-weight-light mt-2 mb-5">Телефон для записи</p>
-                                   <p class="text-secondary font-weight-light mt-2 mb-5">Приём от <del>1400</del> руб.  <span class="text-primary font-weight-bold">1000 руб.</span></p>
+                                                @for($i = 0; $i < 5; $i++)
 
-                               </div>
-                           </div>
+                                                    <img class="star" src="{{ asset('img/star.png') }}" alt="">
+
+                                                @endfor
+                                                <p class="text-secondary mt-3">Превосходный врач<br>на основе 171 отзыв</p>
+                                            </div>
+                                            <div class="col-6">
+                                                <span class="text-secondary h3 mt-5 mb-2">Бобров Василий Елисеевич</span>
+                                                <span class="text-secondary font-weight-light h6 my-3"><em>Гастроэнтеролог, Терапевт <br> Стаж 19 лет</em></span>
+                                                <span class="text-secondary font-weight-light mt-2 mb-5">Приём от <span class="text-primary font-weight-bold">1400 руб.</span><i class="fas fa-exclamation-circle"></i></span>
+                                                <p class="text-secondary font-weight-light mt-2 mb-5">Телефон для записи <br> +996(777) 312-312</p>
+                                                <span class="text-secondary font-weight-light mt-2 mb-5">На прошлой неделе записалось два человека</span>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-3">
+                                        <p class="text-secondary" style="font-size: 0.6rem;">
+                                            Сервис DOCPlus поможет вам выбрать необходимую медицинскую услугу из широкого спектра предоставленных а сайте и записаться в клинику.
+                                        </p>
+                                        <button type="button" class="btn btn-lg btn-info text-light font-weight-bold my-4 shadow text-uppercase h4 py-1 border-bottom" style="border-radius: 50px;">
+                                            Записаться
+                                        </button>
+                                        <p class="text-secondary text-left" style="font-size: 0.6rem;">
+                                            Медицинский центр Иван(MCI)
+                                            Бишкек, ул. Бакча-Ата, д. 45
+                                            Звенигородская (400м)
+                                            Лиговский проспект(300м)
+                                        </p>
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+
                        </div>
+
                    </div>
                </div>
                    </div>
