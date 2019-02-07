18ab5b453d42553d32f1b5a87b60851cf1f3923f
uki7991 <tilek.kubanov@gmail.com>
2019-02-07 16:29:16 +0600

    chat is done

--- resources/views/question/chat.blade.php
+++ resources/views/question/chat.blade.php
@@ -1,7 +1,14 @@
 <section>
     <div class="container">
-        <div class="row">
-            
+        <div class="row justify-content-center">
+            <div class="col-10">
+
+                <div class="chat border rounded p-3 row">
+                    @include('question.partials.question')
+                    @include('question.partials.answer')
+                </div>
+
+            </div>
         </div>
     </div>
 </section>
