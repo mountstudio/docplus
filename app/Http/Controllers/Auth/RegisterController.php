bc5b09046b2fc6f314830b3ae69aeb55e5c447b3
uki7991 <tilek.kubanov@gmail.com>
2019-02-11 05:41:23 +0600

    fix auth

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,6 +63,6 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        User::registerUser($data);
+        return User::registerUser($data);
     }
 }
