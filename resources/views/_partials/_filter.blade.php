c9b530e9afef2ab8968c857903d16d96f79e6bff
Bainurs <Bainurs@users.noreply.github.com>
2019-02-17 23:30:34 +0600

    Create _filter.blade.php

---
+++ resources/views/_partials/_filter.blade.php
@@ -0,0 +1,9 @@
+<div class="row justify-content-center">
+    <div class="col-6 text-center">
+        <div class="btn-group" role="group" aria-label="Basic example">
+            <a href="" class="btn btn-primary active"><i class="fas fa-baby"></i> Детский</a>
+            <a href="" class="btn btn-primary"><i class="fas fa-ambulance"></i> На дом</a>
+
+        </div>
+    </div>
+</div>
