68cbcf1847a711ceb6cd425230191af6676e0d02
Bainurs <Bainurs@users.noreply.github.com>
2019-02-27 18:03:19 +0600

    login register
    delete repcha in register page and login page
    delete coment code in register page
    delete register-patient

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,140 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<!-- <div class="container">
-    <div class="row justify-content-center py-5">
-        <div class="col-md-5 border border-5 bg-light border-primary px-0 rounded">
 
-            <div class="card border-0">
-                <div class="card-header bg-primary text-center text-light font-weight-bold h4">
-                    {{__('Регистрация')}}
-                </div>
-
-                <div class="card-body">
-                    <form>
-
-                        <div class="form-group pt-4 my-4">
-                            <p class="font-weight-bold ">
-                                Кого вы представляете
-                            </p>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-                                <label class="form-check-label" for="exampleRadios1">
-                                    Клинику
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-                                <label class="form-check-label" for="exampleRadios2">
-                                    Врача
-                                </label>
-                            </div>
-                        </div>
-                        <div class="form-group my-4">
-
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text bg-white text-muted" id="basic-addon1"><i class="fas fa-hospital-alt"></i></span>
-                                </div>
-                                <input type="text" class="form-control" placeholder="Название клиники" aria-label="Username" aria-describedby="basic-addon1">
-
-                            </div>
-
-
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text bg-white text-muted" id="basic-addon3"><i class="fas fa-phone"></i></span>
-                                </div>
-                                <input type="text" class="form-control" placeholder="Телефон" aria-label="Username" aria-describedby="basic-addon3">
-
-                            </div>
-
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text bg-white text-muted" id="basic-addon4"><i class="fas fa-envelope"></i></span>
-                                </div>
-                                <input type="text" class="form-control" placeholder="Электронная почта" aria-label="Username" aria-describedby="basic-addon4">
-
-                            </div>
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text bg-white text-muted" id="basic-addon4"><i class="fas fa-key"></i></span>
-                                </div>
-                                <input type="password" class="form-control" placeholder="Пароль" area-label="Password" area-describedby="basic-addon5">
-                            </div>
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text bg-white text-muted" id="basic-addon4"><i class="fas fa-key"></i></span>
-                                </div>
-                                <input type="password" class="form-control" placeholder="Введите пароль ещё раз" area-label="Username" area-describedby="basic-addon6">
-                            </div>
-
-                            <div class="form-group mt-5 ">
-
-                                <div class="g-recaptcha" data-sitekey="0000000000000000000000"></div>
-                                <input type="submit" value="Отправить" class="rss-button btn btn-outline-dark mt-4"/>
-                            </div>
-                        </div>
-
-                    </form>
-                </div>
-            </div>
-
-        </div>
-    </div>
-</div>
-
-<?php
-if(isset($_POST['g-recaptcha-response']) && $_POST['g-recaptcha-response']) {
-    $secret = '0000000000000000000000';
-    $ip = $_SERVER['REMOTE_ADDR'];
-    $response = $_POST['g-recaptcha-response'];
-    $rsp = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$response&remoteip=$ip");
-    //var_dump($rsp);
-    $arr = json_decode($rsp, TRUE);
-    if($arr['success']){
-        if (isset($_POST['name'])) {$name = $_POST['name'];}
-        if (isset($_POST['email'])) {$email = $_POST['email'];}
-        if (isset($_POST['tel'])) {$tel = $_POST['tel'];}
-        if (isset($_POST['comment'])) {$comment = $_POST['comment'];}
-        $mail_header = "MIME-Version: 1.0\r\n";
-        $mail_header.= "Content-type: text/html; charset=UTF-8\r\n";
-        $mail_header.= "From: Any people \r\n";
-        $mail_header.= "Reply-to: Reply to Name \r\n";
-
-        $recipient= 'адрес, кому отправлять';
-        $subject = 'Тема письма';
-        $message = 'Поступила заявка на звонок
-';
-        if (isset($_POST['name'])){
-            $message.= '
-Имя:' . htmlspecialchars($name) . '<br />';
-        }
-        if (isset($_POST['email'])){
-            $message.= '
-Почта:' . htmlspecialchars($email) . '<br />';
-        }
-        if (isset($_POST['tel'])){
-            $message.= '
-Телефон:' . htmlspecialchars($tel) . '<br />';
-        }
-        if (isset($_POST['comment'])){
-            $message.= '
-О проекте: ' . htmlspecialchars($comment) . '<br />';
-        }
-
-
-        if (mail($recipient, $subject, $message, $mail_header))
-            echo 'Письмо отправлено';
-        else echo 'Письмо не отправлено';
-    }
-    else {
-        echo 'Нет';
-    }
-}
-?>
-
- -->
 
 
 <div class="container">
@@ -179,7 +46,7 @@ if(isset($_POST['g-recaptcha-response']) && $_POST['g-recaptcha-response']) {
                     </div>
 
                     <div class="form-group row">
-                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Адрес') }}</label>
 
                         <div class="col-md-6">
                             <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
@@ -193,7 +60,7 @@ if(isset($_POST['g-recaptcha-response']) && $_POST['g-recaptcha-response']) {
                     </div>
 
                     <div class="form-group row">
-                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Пароль') }}</label>
 
                         <div class="col-md-6">
                             <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
@@ -207,7 +74,7 @@ if(isset($_POST['g-recaptcha-response']) && $_POST['g-recaptcha-response']) {
                     </div>
 
                     <div class="form-group row">
-                        <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                        <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Подтвердите пароль') }}</label>
 
                         <div class="col-md-6">
                             <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
