085996069554736fcbd32b2e8823c506a3a00149
uki7991 <tilek.kubanov@gmail.com>
2019-04-02 15:03:10 +0600

    Уменьшил партнеры

--- resources/views/_partials/partners.blade.php
+++ resources/views/_partials/partners.blade.php
@@ -1,19 +1,19 @@
 <div class="container py-5">
-    <div class="row">
+    <div class="row justify-content-center">
         <div class="col-12 mb-3">
             <h2 class="h1 text-center text-uppercase">Клиники партнеры</h2>
         </div>
 
-        <div class="col-3">
+        <div class="col-2">
             <img src="{{ asset('img/boneckii.png') }}" class="img-fluid" alt="">
         </div>
-        <div class="col-3">
+        <div class="col-2">
             <img src="{{ asset('img/boneckii.png') }}" class="img-fluid" alt="">
         </div>
-        <div class="col-3">
+        <div class="col-2">
             <img src="{{ asset('img/boneckii.png') }}" class="img-fluid" alt="">
         </div>
-        <div class="col-3">
+        <div class="col-2">
             <img src="{{ asset('img/boneckii.png') }}" class="img-fluid" alt="">
         </div>
     </div>
