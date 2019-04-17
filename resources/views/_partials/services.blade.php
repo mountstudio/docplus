ff3e1d3b81211456bef6127c85d9bfdad678ccb9
Erkin <mackinkenny@gmail.com>
2019-04-17 23:59:06 +0300

    services not show if doctors is empty

--- resources/views/_partials/services.blade.php
+++ resources/views/_partials/services.blade.php
@@ -1,5 +1,7 @@
 @foreach($services as $service)
+    @if($service->doctors->count() || $service->clinics->count() != 0)
         <li class="nav-item h6 my-0 ">
             <a class="nav-link py-1 text-black" href="/objects/{{$service->id}}">{{$service->name}}</a>
         </li>
+    @endif
 @endforeach
