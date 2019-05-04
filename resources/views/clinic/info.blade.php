e52faa9dabe0d26cd78f3a04d71f8d8242fd37ab
uki7991 <tilek.kubanov@gmail.com>
2019-05-04 19:56:12 +0600

    Update info.blade.php

--- resources/views/clinic/info.blade.php
+++ resources/views/clinic/info.blade.php
@@ -22,7 +22,7 @@
                 {{--</div>--}}
                 {{--<p class="text-muted font-weight-light mb-0 small">Профессиональный рейтинг врача</p>--}}
             </div>
-            <div class="col col-md-auto mt-3 mt-md-0">
+            <div class="col col-md mt-3 mt-md-0">
                 <h3 class="text-secondary text-center text-md-left h2 mt-3 mt-md-0 font-weight-bold">{{ $clinic->type ?? '' }} {{ $clinic->clinic_name }}</h3>
                 <p class="text-secondary font-weight-light m-0 mb-md-2">
                     Телефон для записи: <br>
